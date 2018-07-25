85e83a36150c16edb2ccc294aea0ab8840183491
alexbusol <ovbusol@crimson.ua.edu>
2018-07-25 15:56:56 +0300

    Fixed a bug where no category was showing for the followers/following table view

--- SimplePic/FollowersViewController.swift
+++ SimplePic/FollowersViewController.swift
@@ -15,12 +15,9 @@ class FollowersViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        //showing the category of the table view at the top (following/followers)
+        self.navigationItem.title = showCategory.uppercased()
     }
 
     override func didReceiveMemoryWarning() {

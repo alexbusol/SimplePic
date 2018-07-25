c0493afb184a071f1ec3966b9ad648b9a72be63b
alexbusol <ovbusol@crimson.ua.edu>
2018-07-25 20:23:51 +0300

    Added follow/unfollow functionality to Guest View Now the current user can follow or unfollow another user by clicking on the button in the Guest View

--- SimplePic/HeaderView.swift
+++ SimplePic/HeaderView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class HeaderView: UICollectionReusableView {
         
@@ -23,5 +24,50 @@ class HeaderView: UICollectionReusableView {
     @IBOutlet weak var followingSub: UILabel!
     @IBOutlet weak var profileActionButton: UIButton!
     
+    @IBAction func guestFollowButton_clicked(_ sender: UIButton) {
+        let buttonTitle = profileActionButton.title(for: UIControlState())
+        
+        //if not following -> follow routine
+        if buttonTitle == "Follow" {
+            //creating new object in the follow class
+            let object = PFObject(className: "follow")
+            //placing the current user in the followers of the target user
+            object["follower"] = PFUser.current()?.username
+            object["following"] = guestUsername.last! //placing the target user in the following list of the current user
+            object.saveInBackground(block: { (success, error) -> Void in
+                if success {
+                    self.profileActionButton.setTitle("Following", for: UIControlState())
+                    self.profileActionButton.backgroundColor = .blue
+                } else {
+                    print("There was an error following the user \(error?.localizedDescription)")
+                }
+            })
+        } else { //if following -> unfollow routine
+            let toUnfollow = PFQuery(className: "follow")
+            //looking for the user for which unfollow was clicked
+            toUnfollow.whereKey("follower", equalTo: PFUser.current()!.username!)
+            toUnfollow.whereKey("following", equalTo: guestUsername.last!)
+            //delete the following connection from the database
+            toUnfollow.findObjectsInBackground(block: { (objects, error) -> Void in
+                if error == nil {
+                    
+                    for object in objects! {
+                        object.deleteInBackground(block: { (success, error) -> Void in
+                            if success {
+                                self.profileActionButton.setTitle("Follow", for: UIControlState())
+                                self.profileActionButton.backgroundColor = .lightGray
+                            } else {
+                                print("unable to delete the user following from the DB \(error?.localizedDescription)")
+                            }
+                        })
+                    }
+                    
+                } else {
+                    print("Unable to unfollow \(error?.localizedDescription)")
+                }
+            })
+            
+        }
+    }
     
 }

48ffc2deb9cc7438ce4d868ecab4a318ea662716
alexbusol <ovbusol@crimson.ua.edu>
2018-07-28 16:33:37 +0300

    Updated Post View UI Added layout constraints to the Post View

--- SimplePic/PostCell.swift
+++ SimplePic/PostCell.swift
@@ -26,13 +26,87 @@ class PostCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        configureLayout()
+    }
+    //set constraints programatically
+    func configureLayout() {
+        
+        
+        //disable automatic autoresizing
+        userAvatar.translatesAutoresizingMaskIntoConstraints = false
+        usernameButton.translatesAutoresizingMaskIntoConstraints = false
+        postDateLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        postImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        likeButton.translatesAutoresizingMaskIntoConstraints = false
+        commentButton.translatesAutoresizingMaskIntoConstraints = false
+        moreButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        likeNumLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        uuidLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        //get the current screen's width
+        let pictureWidth = UIScreen.main.bounds.width
+        
+        //set constraints
+        self.contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-10-[ava(30)]-10-[pic(\(pictureWidth))]-5-[like(30)]",
+            options: [], metrics: nil, views: ["ava":userAvatar, "pic":postImage, "like":likeButton]))
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-10-[username]",
+            options: [], metrics: nil, views: ["username":usernameButton]))
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:[pic]-5-[comment(30)]",
+            options: [], metrics: nil, views: ["pic":postImage, "comment":commentButton]))
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-15-[date]",
+            options: [], metrics: nil, views: ["date":postDateLabel]))
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:[like]-5-[title]-5-|",
+            options: [], metrics: nil, views: ["like":likeButton, "title":descriptionLabel]))
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:[pic]-5-[more(30)]",
+            options: [], metrics: nil, views: ["pic":postImage, "more":moreButton]))
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:[pic]-10-[likes]",
+            options: [], metrics: nil, views: ["pic":postImage, "likes":likeNumLabel]))
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-10-[ava(30)]-10-[username]",
+            options: [], metrics: nil, views: ["ava":userAvatar, "username":usernameButton]))
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-0-[pic]-0-|",
+            options: [], metrics: nil, views: ["pic":postImage]))
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-15-[like(30)]-10-[likes]-125-[comment(30)]",
+            options: [], metrics: nil, views: ["like":likeButton, "likes":likeNumLabel, "comment":commentButton]))
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "H:[more(30)]-15-|",
+            options: [], metrics: nil, views: ["more":moreButton]))
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-15-[title]-15-|",
+            options: [], metrics: nil, views: ["title":descriptionLabel]))
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|[date]-10-|",
+            options: [], metrics: nil, views: ["date":postDateLabel]))
+        
+        //round the avatar near the username
+        userAvatar.layer.cornerRadius = userAvatar.frame.size.width / 2
+        userAvatar.clipsToBounds = true
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
 }

d119da02f52eb8cbd3d939f948dd27e3014773cb
alexbusol <ovbusol@crimson.ua.edu>
2018-11-02 18:57:21 -0500

    Added tab bar icons. Created Notification View Controller that will be responsible for displaying notifications when a user's post has been liked, a user got a new follower, or was mentioned using @ feature.

---
+++ SimplePic/NotificationCell.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationCell.swift
+//  SimplePic
+//
+//  Created by Alex Busol on 11/2/18.
+//  Copyright © 2018 Alex Busol. All rights reserved.
+//
+
+import UIKit
+
+class NotificationCell: UITableViewCell {
+
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var usernameButton: UIButton!
+    @IBOutlet weak var informationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
